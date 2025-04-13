44ba0caed0c8ace36e372e20a886578d6aacbb88
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2025-04-13 17:30:10 +0900

    Apr-13

--- src/components/SideMenu/NavList/NavItem/NavItem.tsx
+++ src/components/SideMenu/NavList/NavItem/NavItem.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 interface NavItemProps {
@@ -10,11 +13,18 @@ interface NavItemProps {
 const NavItem: React.FC<NavItemProps> = ({
     label, link, icon
 }) => {
+    const pathname = usePathname();
     return (
-        <Link href={link}>
-            <div>{icon}</div>
+        <Link
+            href={link}
+            className={`flex p-4 items-center
+        w-full hover:bg-gray-700 font-medium
+        ${pathname === link ? 'bg-gray-600 border-r-4 border-r-green-500' : ''}`}
+        >
+            <div className='mr-1'>{icon}</div>
+            <div>{label}</div>
         </Link>
-    )
+    );
 }
 
 export default NavItem
--- src/components/SideMenu/NavList/NavList.tsx
+++ src/components/SideMenu/NavList/NavList.tsx
@@ -1,4 +1,5 @@
 import { FaRegCheckSquare, FaRegClock, FaTasks } from "react-icons/fa";
+import NavItem from "./NavItem/NavItem";
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 interface NavItemType {
@@ -10,15 +11,35 @@ interface NavItemType {
 
 const NavList = () => {
     const navList: NavItemType[] = [
-        { id: 1, label: 'All Tasks', link: '/', icon: <FaTasks className='size-5' /> },
-        { id: 2, label: 'Completed Tasks', link: '/completed', icon: <FaRegCheckSquare className='size-5' /> },
-        { id: 3, label: 'Expired Tasks', link: '/expired', icon: <FaRegClock className='size-5' /> }
+        {
+            id: 1,
+            label: 'All Tasks',
+            link: '/',
+            icon: <FaTasks className='size-5' />
+        },
+        {
+            id: 2,
+            label: 'Completed Tasks',
+            link: '/completed',
+            icon: <FaRegCheckSquare className='size-5' />
+        },
+        {
+            id: 3,
+            label: 'Expired Tasks',
+            link: '/expired',
+            icon: <FaRegClock className='size-5' />
+        }
     ];
     return (
         <div className='mt-24'>
-            <div>Link1</div>
-            <div>Link2</div>
-            <div>Link3</div>
+            {navList.map((item) => (
+                <NavItem
+                    key={item.id}
+                    label={item.label}
+                    link={item.link}
+                    icon={item.icon}
+                />
+            ))}
         </div>
     )
 }
