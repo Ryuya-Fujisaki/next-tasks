d6a9672066623a57ac492f8ba4eae5f41933a7e5
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2025-04-14 23:58:57 +0900

    Message change to Rocky

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -22,7 +22,7 @@ export default async function MainPage() {
   return (
     <div className="text-gray-800 p-8 h-full overflow-y-auto pb-24">
       <header className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold flex items-center">All Tasks</h1>
+        <h1 className="text-2xl font-bold flex items-center">All Of America</h1>
         <Link
           href="/new"
           className="flex items-center gap-1 font-semibold border px-4
