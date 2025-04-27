650ad47b0b9ee379a1584c3a0d960d32525e7717
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2025-04-27 17:54:31 +0900

    MongoDB connection configration

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import mongoose from "mongoose";
 
 export const connectDb = async () => {
     try {
-await mongoose.connect()
+await mongoose.connect(process.env.DB_URI || '')
     } catch (error) {
-
+        console.log('DB接続に失敗しました');
+        throw new Error();
     }
 }
