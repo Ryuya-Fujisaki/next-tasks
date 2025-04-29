3253f90838587bb60ecca91b2069168e8eb89b82
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2025-04-29 13:05:42 +0900

    ローディングコンポーネントの実装

--- src/components/TaskCard/TaskDeleteButton/TaskDeleteButton.tsx
+++ src/components/TaskCard/TaskDeleteButton/TaskDeleteButton.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+import { deleteTask, FormState } from "@/actions/task";
+import { useEffect } from "react";
+import { useFormState, useFormStatus } from "react-dom";
 import { FaTrashAlt } from "react-icons/fa";
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
@@ -6,12 +11,34 @@ interface TaskDeleteButtonProps {
 }
 
 const TaskDeleteButton: React.FC<TaskDeleteButtonProps> = ({ id }) => {
-    return (
-        <form action="">
-            <button type='submit' className='hoveer:text-gray-700
-            text-lg cursor-pointer'>
+    const deleteTaskWithId = deleteTask.bind(null, id);
+    const initialState: FormState = { error: '' };
+    const [state, formAction] = useFormState(deleteTaskWithId, initialState);
+
+    useEffect(() => {
+        if (state && state.error !== '') {
+            alert(state.error);
+        }
+    }, [state])
+
+    const SubmitButton = () => {
+        const { pending } = useFormStatus();
+
+        return (
+            <button
+                type='submit'
+                disabled={pending}
+                className='hoveer:text-gray-700
+            text-lg cursor-pointer disabled:bg-gray-400'
+            >
                 <FaTrashAlt />
             </button>
+        )
+
+    }
+    return (
+        <form action={formAction}>
+            <SubmitButton />
         </form>
     )
 }
